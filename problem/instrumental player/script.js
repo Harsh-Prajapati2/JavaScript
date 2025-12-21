cb7052692c0dee23c51ac26a5f07093ca1feaa25
Harsh-Prajapati2 <harshprajapati020105@gmail.com>
2025-12-21 10:03:24 +0530

    update in instrumental player

--- problem/instrumental player/script.js
+++ problem/instrumental player/script.js
@@ -1,4 +1,4 @@
-// const w = document.getElementsByClassName('w');
+const w = document.getElementsByClassName('w');
 // const a = document.getElementsByClassName('a');
 // const s = document.getElementsByClassName('s');
 // const d = document.getElementsByClassName('d');
@@ -14,22 +14,22 @@ document.addEventListener('keypress',function(event){
     console.log("w is pressed");
     new Audio('./sounds/tom-1-28537.mp3').play();
   }else if(event.key=='a'){
-    console.log("w is pressed");
+    console.log("a is pressed");
     new Audio('./sounds/tom-2-28541.mp3').play();
   }else if(event.key=='s'){
-    console.log("w is pressed");
+    console.log("s is pressed");
     new Audio('./sounds/tom-3-28542.mp3').play();
   }else if(event.key=='d'){
-    console.log("w is pressed");
+    console.log("d is pressed");
     new Audio('./sounds/tom-4-28543.mp3').play();
   }else if(event.key=='j'){
-    console.log("w is pressed");
+    console.log("j is pressed");
     new Audio('./sounds/crash-28546.mp3').play();
   }else if(event.key=='k'){
-    console.log("w is pressed");
+    console.log("k is pressed");
     new Audio('./sounds/kick-bass-28547.mp3').play();
   }else if(event.key=='l'){
-    console.log("w is pressed");
+    console.log("l is pressed");
     new Audio('./sounds/snare-28545.mp3').play();
   }else{
     console.log("Enter key which is shown.");
